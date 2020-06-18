302b9897db0587d6d184c545fc5eb7d65cde8d26
Gabriel Cencic <gabriel.cencic@gmail.com>
2020-06-18 01:31:15 -0300

    Hint texts with animations

---
+++ Assets/Scripts/Hint.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hint : MonoBehaviour
+{
+
+    // public Animator textAnimator;
+    public CanvasGroup uiElement;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FadeIn();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FadeOut();
+        }
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1, .5f));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 0, .5f));
+    }
+
+    public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 1)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+
+            if (percentageComplete >= 1) break;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 Unity mini experiments series: basic platformer with common 2D mechanics.
 
+Live demo:
+https://gcadeado.github.io/unity-platformer-experiments/
+
 ## Experiments
 
 - Custom player controller and movement mechanics
 - Level designs using tilemaps
 - Extended 2D tools with [2d-extras](https://github.com/Unity-Technologies/2d-extras)
 - 2D animations and controllers
+- TextMeshPro with custom script animations
 
 ### Credits
 
